0cb4d723a1c0bba7e2d79ecd797176ef0828af27
crjeffrey <105098993+crjeffrey@users.noreply.github.com>
2022-05-15 13:13:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -332,55 +332,50 @@
 
 })(jQuery);
 
-( function($) {
-  
-  $(document).ready(function() {
-    
-    var s           = $('.slider'),
-        sWrapper    = s.find('.slider-wrapper'),
-        sItem       = s.find('.slide'),
-        btn         = s.find('.slider-link'),
-        sWidth      = sItem.width(),
-        sCount      = sItem.length,
-        slide_date  = s.find('.slide-date'),
-        slide_title = s.find('.slide-title'),
-        slide_text  = s.find('.slide-text'),
-        slide_more  = s.find('.slide-more'),
-        slide_image = s.find('.slide-image img'),
-        sTotalWidth = sCount * sWidth;
-    
-    sWrapper.css('width', sTotalWidth);
-    sWrapper.css('width', sTotalWidth);
-    
-    var clickCount  = 0;
-    
-    btn.on('click', function(e) {
-      e.preventDefault();
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
 
-      if( $(this).hasClass('next') ) {
-        
-        ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
-      } else if ( $(this).hasClass('prev') ) {
-        
-        ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
-      }
-      TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
 
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
 
-      //CONTENT ANIMATIONS
-
-      var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
-      var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
-
-      TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
-      TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
-      TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
-      TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
-      TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
-
-    });
-          
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
-})(jQuery);
-
-$('.overlay').addClass('overlay-blue');
+}
